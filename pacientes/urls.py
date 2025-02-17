8aafe5b182408900681adbc83f1e52be4ecfa641
Isabella Arão <166514758+araoisabella@users.noreply.github.com>
2025-02-16 21:25:23 -0300

    Add files via upload

---
+++ pacientes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.pacientes, name="pacientes"), 
+    path('<int:id>', views.paciente_view, name="paciente_view"),
+    path('atualizar_paciente/<int:id>', views.atualizar_paciente, name="atualizar_paciente"),
+    path('excluir_consulta/<int:id>', views.excluir_consulta, name="excluir_consulta"),
+    path('consulta_publica/<int:id>', views.consulta_publica, name="consulta_publica"),
+]
